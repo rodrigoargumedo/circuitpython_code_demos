0faa22df04a4bc5aa5c40be3de7606dc5149f266
Rodrigo Argumedo <7613139+rodrigoargumedo@users.noreply.github.com>
2021-05-09 22:25:04 -0500

    Init: Service test for send_batch_data to Adafruit IO.

---
+++ adafruit_io_send_batch_data_test.py
@@ -0,0 +1,12 @@
+# Copyright 2021 Rodrigo Argumedo
+# License: MIT
+
+import board
+import busio
+from digitalio import DigitalInOut
+import adafruit_esp32spi.adafruit_esp32spi_socket as socket
+from adafruit_esp32spi import adafruit_esp32spi
+import adafruit_requests as requests
+from adafruit_io.adafruit_io import IO_HTTP
+
+# TODO: Add Python Code Implementation
